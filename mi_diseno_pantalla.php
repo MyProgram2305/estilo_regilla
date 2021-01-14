234fe3455a97d88d70248a9f12c556c11b0ec4ca
myprogram2305 <75906602+MyProgram2305@users.noreply.github.com>
2021-01-13 19:51:08 -0600

    los estilos del div

---
+++ mi_diseno_pantalla.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title> Mi pantalla </title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3" style="border: 2px solid pink">
+				<h1> primera parte </h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+			</div>
+
+
+			<div class="col-md-9" style="border: 2px solid red">
+				<h1> segunda parte </h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+
+	    </div>
+	</div>
+
+	 <script src="js/jquery-1.11.1.js"></script>
+	 <script src="js/bootstrap.min.js"></script>
+	 <img src="img/taco.jpeg" width="50">
+</body>
+</html>
